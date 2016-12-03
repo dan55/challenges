85220f2bd9cd56c3f842b85fa0202ed965b3bffd
dan55 <dan.elkin85@gmail.com>
2016-12-03 16:08:55 -0500

    add tests

--- linked_list_cycle.py
+++ linked_list_cycle.py
@@ -1,5 +1,4 @@
 '''
-
 Write a function that returns True if a linked list contains a cycle and False otherwise.
 
 We use two pointers that traverse the list at different speeds. Imagine yourself and Usain Bolt
@@ -15,19 +14,15 @@ https://www.interviewcake.com/question/python/linked-list-cycles
 https://www.hackerrank.com/challenges/ctci-linked-list-cycle
 
 '''
-
-class Node(object):
-
-	def __init__(self, val):
-		self.val = val
-		self.next = None
-
+import unittest
 
 def has_cycle(head):
 	fast_ptr = head
 	slow_ptr = head
 
-	while fast_ptr and fast_ptr.next:
+	# Is fast_ptr either just passed the last node 
+	# or are at the last node?
+	while fast_ptr and fast_ptr.next: 
 		slow_ptr = slow_ptr.next
 		fast_ptr = fast_ptr.next.next
 
@@ -36,19 +31,33 @@ def has_cycle(head):
 	
 	return False
 
-def main():
-	head = Node(1)
-	ptr = head
 
-	for i in range(2, 10):
-		ptr.next = Node(i)
-		ptr = ptr.next
+class Node(object):
 
-	print(has_cycle(head))
+	def __init__(self, val):
+		self.val = val
+		self.next = None
 
-	ptr.next = head
 
-	print(has_cycle(head))
+class TestHasCycle(unittest.TestCase):
+	
+	def setUp(self):
+		self.head = Node(1)
+		ptr = self.head
+
+		for i in range(2, 10):
+			ptr.next = Node(i)
+			ptr = ptr.next
+
+		self.tail = ptr
+
+	def test_list_without_cycle(self):
+		self.assertFalse(has_cycle(self.head))
+
+	def test_list_with_cycle(self):
+		self.tail.next = self.head
+
+		self.assertTrue(has_cycle(self.head))
 
 if __name__ == '__main__':
-	main()
+	unittest.main()
