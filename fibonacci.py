f07744d6dd36f93af2ad5c55cf84f4a1b0174169
Dan <dan.elkin85@gmail.com>
2016-12-02 22:43:15 -0500

    rabbits

---
+++ fibonacci.py
@@ -0,0 +1,37 @@
+import itertools 
+
+def iterative():
+	a, b = 1, 1
+
+	while True:
+		yield a
+
+		a, b = b, a + b
+
+
+def recursive(n):
+	if n < 2:
+		return n 
+
+	return recursive(n - 1) + recursive(n - 2)
+
+
+def memoized(n, cache={}):
+	if n < 2:
+		return n
+
+	if n in cache:
+		return cache[n]
+
+	cache[n] = memoized(n - 1) + memoized(n - 2)
+
+	return cache[n]
+
+
+def main():
+	n = 30
+
+	print(list(itertools.islice(iterative(), n)).pop() == recursive(n) == memoized(n))
+
+if __name__ == '__main__':
+	main()
