fee1eecf56d73925c9c7f12a6d20d51ad13c204b
Dan <dan.elkin85@gmail.com>
2016-12-05 21:25:46 -0500

    unique_number.py

---
+++ unique_number.py
@@ -0,0 +1,33 @@
+'''
+Problem:
+
+Find the unique number in an array where all other numbers are pairs.
+
+Can be done in theta(n) time.
+
+
+Thoughts:
+
+Any number xor'ed with itself is 0. Any number xor'ed with 0 is that number.
+
+Thus, we walk through the list, and all of the pairs cancel to 0, leaving
+
+the number we're searching for.
+
+
+Sources:
+
+https://www.interviewcake.com/question/python/find-unique-int-among-duplicates
+
+https://www.hackerrank.com/challenges/ctci-lonely-integer
+'''
+
+def get_unique_num(arr):
+    target = 0
+
+    for num in arr:
+        target ^= num
+
+    return target
+
+print(get_unique_num([8, 34, 12, 12, 8]) == 34) 
